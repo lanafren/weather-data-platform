4afa3ac8d573c4171fa85fd4b7bda93a1bcb6192
Lana Frenzel <166107394+lanafrenzel@users.noreply.github.com>
2025-11-17 12:55:40 +0100

    Model update: current_raw.sql and sources.yml

---
+++ dbt/models/current_raw.sql
@@ -0,0 +1,29 @@
+select
+    datetime(
+    timestamp_seconds(cast(json_value(data, '$.dt') as int64)),
+    "Europe/Berlin") as record_time,
+
+    datetime(
+    timestamp_seconds(cast(json_value(data, '$.sys.sunrise') as int64)),
+    'Europe/Berlin') as sunrise,
+
+    datetime(timestamp_seconds(cast(json_value(data, '$.sys.sunset') as int64)),
+    'Europe/Berlin') as sunset,
+
+    cast(json_value(data, '$.main.temp') as float64) as temp,
+    cast(json_value(data, '$.main.feels_like') as float64) as feels_like,
+    json_value(data, '$.weather[0].main') as weather_main,
+    json_value(data, '$.weather[0].description') as weather_description,
+    cast(json_value(data, '$.main.pressure') as float64) as pressure_hpa,
+    cast(json_value(data, '$.main.humidity') as float64) as humidity,
+    cast(json_value(data, '$.visibility') as float64) as visibility_m,
+    cast(json_value(data, '$.clouds.all') as float64) as clouds_pct,
+    cast(json_value(data, '$.wind.speed') as float64) as wind_speed,
+    cast(json_value(data, '$.wind.deg') as float64) as wind_deg,
+    cast(json_value(data, '$.wind.gust') as float64) as wind_gust,
+    cast(json_value(data, '$.rain.1h') as float64) as rain_1h_mm,
+    cast(json_value(data, '$.snow.1h') as float64) as snow_1h_mm,
+    json_value(data, '$.rain.1h') is not null as is_raining,
+    json_value(data, '$.snow.1h') is not null as is_snowing,
+    fetched_at
+from {{ source("raw_data", "current_raw") }}
